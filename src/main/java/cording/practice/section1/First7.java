6d526e5e3edee1b2e0c6c1ab3db5d078615c50dc
jin <jintyjin15@gmail.com>
2022-03-15 01:17:43 +0900

    Section1 String 6 ~ 7

---
+++ src/main/java/cording/practice/section1/First7.java
@@ -0,0 +1,56 @@
+package cording.practice.section1;
+
+import java.util.Scanner;
+
+/**
+ * 7. 회문 문자열
+
+ * 설명
+ * 앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 회문 문자열이라고 합니다.
+ * 문자열이 입력되면 해당 문자열이 회문 문자열이면 "YES", 회문 문자열이 아니면 “NO"를 출력하는 프로그램을 작성하세요.
+ * 단 회문을 검사할 때 대소문자를 구분하지 않습니다.
+ *
+ * 입력
+ * 첫 줄에 길이 100을 넘지 않는 공백이 없는 문자열이 주어집니다.
+ *
+ * 출력
+ * 첫 번째 줄에 회문 문자열인지의 결과를 YES 또는 NO로 출력합니다.
+ *
+ * 예시 입력 1
+ * gooG
+ *
+ * 예시 출력 1
+ * YES
+ */
+
+public class First7 {
+    public static String solution(String str) {
+        String answer = "";
+
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equalsIgnoreCase(reverse)) {
+            answer = "YES";
+        } else {
+            answer = "NO";
+        }
+//        String reverse = new StringBuilder(str).reverse().toString();
+//
+//        if (str.equalsIgnoreCase(reverse)) {
+//            answer = "YES";
+//        } else {
+//            answer = "NO";
+//        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        String str = in.next();
+        System.out.println(solution(str));
+    }
+}
