e51ea2719226663f395bd8b5025d0c5fedf6752f
jin <jintyjin15@gmail.com>
2022-05-04 18:09:07 +0900

    Section8 DFS, BFS uses7

---
+++ src/main/java/cording/practice/section8/Eighth7.java
@@ -0,0 +1,24 @@
+package cording.practice.section8;
+
+import java.util.Scanner;
+
+public class Eighth7 {
+    static int[][] dy = new int[35][35];
+
+    public static int DFS(int n, int r) {
+        if (dy[n][r] > 0) {
+            return dy[n][r];
+        }
+        if (n == r || r == 0) {
+            return 1;
+        } else {
+            return dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        System.out.println(DFS(n, r));
+    }
+}
