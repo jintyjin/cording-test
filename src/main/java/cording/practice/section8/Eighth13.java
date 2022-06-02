0b4497d26d77c2b46b5fcc976644a8977e22ce5e
jin <jintyjin15@gmail.com>
2022-06-02 20:12:44 +0900

    Section8 DFS, BFS uses 13 ~ 14

---
+++ src/main/java/cording/practice/section8/Eighth13.java
@@ -0,0 +1,44 @@
+package cording.practice.section8;
+
+import java.util.Scanner;
+
+public class Eighth13 {
+    static int n;
+    static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+    static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+    static int answer = 0;
+
+    public static void DFS(int x, int y, int[][] board) {
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1) {
+                board[nx][ny] = 0;
+                DFS(nx, ny, board);
+            }
+        }
+    }
+    public static void solution(int[][] board) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1) {
+                    answer++;
+                    board[i][j] = 0;
+                    DFS(i, j, board);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        solution(arr);
+        System.out.println(answer);
+    }
+}
